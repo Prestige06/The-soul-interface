85d7a3ace5ef3aa30726eafceefeef8c648e2043
Prestige06 <115601062+Prestige06@users.noreply.github.com>
2025-04-08 10:13:47 +0100

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logo}>Arkadia</div>
+      <ul className={styles.menu}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/aelera">Aelera</Link></li>
+        <li><Link href="/explore">Explore</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+    }
