9641dd57a5a6c4124753743317dad8a08ca4de1f
Prestige06 <115601062+Prestige06@users.noreply.github.com>
2025-04-08 07:50:54 +0100

    Create aelera.js

---
+++ components/aelera.js
@@ -0,0 +1,7 @@
+export default function Aelera() {
+  return (
+    <div className="aelera-guide">
+      <p>"Trust in the journey, and all will be revealed at the right time."</p>
+    </div>
+  );
+}
