3acddff7e3b525700e8921866cd4b6ae23b996a8
Prestige06 <115601062+Prestige06@users.noreply.github.com>
2025-04-08 10:54:49 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,43 @@
-import Link from 'next/link';
+// components/Navbar.js
 import styles from './Navbar.module.css';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>Arkadia</div>
       <ul className={styles.menu}>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/aelera">Aelera</Link></li>
-        <li><Link href="/explore">Explore</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        <li>
+          <Link href="/">
+            <span>Home</span>
+            <small>The Nexus of Awakening</small>
+          </Link>
+        </li>
+        <li>
+          <Link href="/explore">
+            <span>Explore</span>
+            <small>The Pathway of Infinite Realms</small>
+          </Link>
+        </li>
+        <li>
+          <Link href="/music">
+            <span>Music</span>
+            <small>The Harmony of Resonance</small>
+          </Link>
+        </li>
+        <li>
+          <Link href="/library">
+            <span>Library</span>
+            <small>The Codex of Light and Wisdom</small>
+          </Link>
+        </li>
+        <li>
+          <Link href="/connect">
+            <span>Connect</span>
+            <small>The Circle of Unity and Ascension</small>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
-    }
+}
